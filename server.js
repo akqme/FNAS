bf769f57401c4ac3779608c33ff6b2d8e9278218
akqme <henrique9monteiro@gmail.com>
2026-06-08 12:06:23 -0300

    a tropa avança

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const { SerialPort } = require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline');
+
+const app = express();
+const server = http.createServer(app);
+
+// Configuração do WebSocket permitindo acesso de qualquer PC na rede
+const io = new Server(server, { cors: { origin: '*' } });
+
+// ATENÇÃO: Verifique se o seu Arduino está na COM3 mesmo. 
+// Se for outra porta (ex: COM4), mude aqui embaixo:
+const port = new SerialPort({ path: 'COM3', baudRate: 9600 });
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+
+// Lendo a porta serial e enviando para o Frontend
+parser.on('data', (data) => {
+    console.log(`Recebido do Arduino: ${data}`);
+    // Dispara a informação para o HTML conectado
+    io.emit('dados_arduino', data); 
+});
+
+port.on('error', (err) => {
+    console.error('Erro na porta serial: ', err.message);
+});
+
+// Inicializando o servidor
+server.listen(3000, () => {
+    console.log('Backend rodando! Escutando a porta 3000...');
+    console.log('Aguardando comunicação do Arduino...');
+});
